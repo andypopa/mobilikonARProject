2cdf13d4c1fa6fdf23e5b053d04d0832f2ae132c
Amir <biglariamirhossein94@gmail.com>
2018-03-05 16:18:34 +0200

    FirstCommit by Sourcetree

---
+++ Source/HelloARUnreal/Public/CreateSVG.h
@@ -0,0 +1,28 @@
+// // Copyright 2017 Google Inc.//// Licensed under the Apache License, Version 2.0 (the "License");// you may not use this file except in compliance with the License.// You may obtain a copy of the License at//// http://www.apache.org/licenses/LICENSE-2.0//// Unless required by applicable law or agreed to in writing, software// distributed under the License is distributed on an "AS IS" BASIS,// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.// See the License for the specific language governing permissions and// limitations under the License.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CreateSVG.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLOARUNREAL_API UCreateSVG : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+		UFUNCTION(BlueprintCallable, Category = "CreateSVG", meta = (Keywords = "CreateSVG"))
+		void CreateSVGFunction();
+
+	UFUNCTION(BlueprintCallable, Category = "save")
+		static bool FileSaveString(FString SaveTextB, FString FileNameB);
+
+	UFUNCTION(BlueprintPure, Category = "save")
+		static bool FileLoadString(FString FileNameA, FString& SaveTextA);
+
+};
